5e60b9ffd15a31c7c3e663810f2cb3670a1d6881
DualDrive-1 <dualdrive001@gmail.com>
2025-08-19 01:06:25 +0300

    Add EF InitialCreate migratio1

---
+++ Data/Migrations/20250818220559_SyncModel_20250819.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JaeZoo.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel_20250819 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
